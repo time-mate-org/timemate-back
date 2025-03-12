7f7b8ca249e2a2656202f50339ea703de108cfea
Gustavo-Ennes <gustavo.ennes@3778.care>
2025-03-12 14:22:50 -0300

    Project name fix

--- config.py
+++ config.py
@@ -3,8 +3,9 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     app_name: str = "Team Mate"
-    admin_email: str = 'teammate@ennes.dev'
+    admin_email: str = 'timemate@ennes.dev'
     db_connectin: str = ''
+    # exportar no ambiente e usar variável de ambiente
     hashing_secret: str = '8064be1db0b010b31844b0da5c8f29dc63db18453e5377f59dadb0c2d9463e88'
 
 
