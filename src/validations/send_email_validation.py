207fcca31316f47b97f4f85a26f8e94e77768a21
Gustavo-Ennes <gustavo.ennes@3778.care>
2025-04-23 09:29:15 -0300

    Fix email sending parameters and template

--- src/validations/send_email_validation.py
+++ src/validations/send_email_validation.py
@@ -1,13 +1,14 @@
 from pydantic import BaseModel
 
+
 class Contact(BaseModel):
     name: str
     email: str
-    
+
+
 class SendMailValidation(BaseModel):
     category: str
     content: str
     subject: str
-    subtitle: str
     to: Contact
     origin: Contact
