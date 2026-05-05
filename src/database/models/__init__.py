ddaa82ee62f401bf0a54aec711144a982b23be5e
Gustavo Ennes <gustavo@ennes.dev>
2026-05-05 10:14:44 -0300

    User table

--- src/database/models/__init__.py
+++ src/database/models/__init__.py
@@ -2,5 +2,6 @@ from .service import Service
 from .client import Client
 from .professional import Professional
 from .appointment import Appointment
+from .user import User
 from .tenant import Tenant
 from .base import Base
